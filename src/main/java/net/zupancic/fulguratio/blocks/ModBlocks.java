2c40c1143906e5a38da2b9ab1da5bda7a9b6a51a
c-ola <nikola.z37@hotmail.com>
2023-01-11 22:53:01 -0500

    Added basic functionality for recipes, loot tables, etc and added some textures for blocks.

--- src/main/java/net/zupancic/fulguratio/blocks/ModBlocks.java
+++ src/main/java/net/zupancic/fulguratio/blocks/ModBlocks.java
@@ -2,9 +2,11 @@ package net.zupancic.fulguratio.blocks;
 
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,19 +17,24 @@ import net.zupancic.fulguratio.items.ModItems;
 public class ModBlocks {
     //Contains all blocks from the mod
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Fulguratio.MODID);
-    public static final Material HARD_ESSENCE = (new Material.Builder(MaterialColor.NONE)).build();
+    public static final Material HARD_ESSENCE = (new Material.Builder(MaterialColor.STONE)).build();
 
     //Every added block goes here (not sure about tile entities yet)
     //Each block has an item counter part
     public static final RegistryObject<Block> CRYSTALARIUM_BLOCK = 
         BLOCKS.register("crystalarium_block", () -> new Block(BlockBehaviour.Properties.of(Material.GLASS)));
     public static final RegistryObject<Item> CRYSTALARIUM_BLOCK_ITEM = 
-        ModItems.ITEMS.register("crystalarium_block_item", () -> new BlockItem(CRYSTALARIUM_BLOCK.get(), new Item.Properties()));
+        ModItems.ITEMS.register("crystalarium_block", () -> new BlockItem(CRYSTALARIUM_BLOCK.get(), new Item.Properties()));
     
     public static final RegistryObject<Block> FULGURIUM_BLOCK = 
-        BLOCKS.register("fulgurium_block", () -> new Block(BlockBehaviour.Properties.of(HARD_ESSENCE)));
+        BLOCKS.register("fulgurium_block", () -> new Block(BlockBehaviour.Properties.of(HARD_ESSENCE).strength(1f)));
     public static final RegistryObject<Item> FULGURIUM_BLOCK_ITEM = 
-        ModItems.ITEMS.register("fulgurium_block_item", () -> new BlockItem(FULGURIUM_BLOCK.get(), new Item.Properties()));
+        ModItems.ITEMS.register("fulgurium_block", () -> new BlockItem(FULGURIUM_BLOCK.get(), new Item.Properties()));
+
+    public static final RegistryObject<Block> FULGURIUM_ORE = 
+        BLOCKS.register("fulgurium_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).requiresCorrectToolForDrops().strength(0.7f), UniformInt.of(3, 7)));
+    public static final RegistryObject<Item> FULGURIUM_ORE_ITEM = 
+        ModItems.ITEMS.register("fulgurium_ore", () -> new BlockItem(FULGURIUM_ORE.get(), new Item.Properties()));
 
 
 }
